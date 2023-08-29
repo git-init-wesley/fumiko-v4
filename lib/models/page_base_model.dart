bcae8388446cdc4db1941d8f8fb1f9b6098020ad
Wesley LEVASSEUR <mailpro.levasseur@icloud.com>
2023-08-29 21:47:15 +0200

    Add models

---
+++ lib/models/page_base_model.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:fumiko/core/core.dart';
+import 'package:fumiko/l10n/l10n.dart';
+import 'package:fumiko/router/services/router_services.dart';
+
+class PageBaseModel extends ChangeNotifier {
+  final RouterNavigationService _navigationService =
+      RouterService.instance.locator<RouterNavigationService>();
+
+  RouterNavigationService get navigationService => _navigationService;
+
+  Future<void> setState(Function() function) async {
+    await function();
+    notifyListeners();
+  }
+
+  Future<dynamic> changeLanguage(Locale locale) async {
+    await setState(() async {
+      await Core.instance.sharedPreferences!
+          .setString('fumiko-locale', locale.toLanguageTag());
+      await AppLocalizations.delegate.load(locale);
+    });
+  }
+
+  Future<String> get currentLocale => Core.instance.currentLocale;
+}
