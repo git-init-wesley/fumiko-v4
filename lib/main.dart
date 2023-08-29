65d4486d6f33cac3139a354c37faf8ab3f01aa47
Wesley LEVASSEUR <mailpro.levasseur@icloud.com>
2023-08-29 21:48:03 +0200

    Add/Imports main

---
+++ lib/main.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:fumiko/l10n/l10n.dart';
+import 'package:fumiko/router/services/router_services.dart';
+
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Fumiko',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          brightness: Brightness.dark,
+          primarySwatch: Colors.red,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+          splashColor: Colors.red,
+          highlightColor: Colors.red,
+          scrollbarTheme: ScrollbarThemeData(
+            interactive: true,
+            radius: const Radius.circular(16),
+            thumbColor: MaterialStateProperty.all(Colors.red[400]),
+            thickness: MaterialStateProperty.all(5),
+          ),
+          fontFamily: 'Dosis',
+          tooltipTheme: const TooltipThemeData(
+              decoration: ShapeDecoration(
+            color: Color(0xFF232426),
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.all(Radius.circular(16))),
+          )),
+        ),
+        themeMode: ThemeMode.light,
+        localizationsDelegates: const [
+          AppLocalizations.delegate,
+          GlobalMaterialLocalizations.delegate,
+          GlobalWidgetsLocalizations.delegate,
+          GlobalCupertinoLocalizations.delegate,
+        ],
+        onGenerateRoute: RouterService.instance.onGenerateRoute,
+        navigatorKey: RouterService.instance
+            .locator<RouterNavigationService>()
+            .navigatorKey,
+        supportedLocales: AppLocalizations.delegate.supportedLocales);
+  }
+}
