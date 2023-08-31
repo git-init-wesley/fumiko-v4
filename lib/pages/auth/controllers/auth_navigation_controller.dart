0c7e3961ab9c3443d033d7746edb5d1c1837f7fe
Wesley LEVASSEUR <mailpro.levasseur@icloud.com>
2023-08-31 17:21:20 +0200

    Update AuthNavigationController

--- lib/pages/auth/controllers/auth_navigation_controller.dart
+++ lib/pages/auth/controllers/auth_navigation_controller.dart
@@ -4,8 +4,7 @@ import 'package:fumiko/router/services/router_services.dart';
 mixin AuthNavigationController {
   RouterNavigationService get authNavigationService;
 
-  //TODO: Back to really
-  VoidCallback get navigateToRecoveryPassword => () async => authNavigationService.pushReplacementTo(RouterRoutes.authSignIn);
+  VoidCallback get navigateToRecovery => () async => authNavigationService.pushReplacementTo(RouterRoutes.authRecovery);
 
   VoidCallback get navigateToSignUp => () async => authNavigationService.pushReplacementTo(RouterRoutes.authSignUp);
 
