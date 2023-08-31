f350e94fb825670d7ecb7c71ed1d167af318c845
Wesley LEVASSEUR <mailpro.levasseur@icloud.com>
2023-08-31 13:11:13 +0200

    Update exceptions.auth

--- lib/exceptions/auth/auth_exceptions.dart
+++ lib/exceptions/auth/auth_exceptions.dart
@@ -3,11 +3,13 @@ part of app_exceptions;
 class AuthExceptions {
   AuthExceptions._();
 
-  static List<AppException> get authErrors => [userNotFound(), userDisabled(), invalidEmail(), emailAlreadyInUse(), wrongPassword(), usernameAlreadyTaken()];
+  static List<AppException> get authErrors => [userNotFound(), userDisabled(), tooManyRequests(), invalidEmail(), emailAlreadyInUse(), wrongPassword(), usernameAlreadyTaken()];
 
   static AppException userNotFound() => AppException(code: 'user-not-found', message: 'User not found.');
 
   static AppException userDisabled() => AppException(code: 'user-disabled', message: 'User disabled.');
+
+  static AppException tooManyRequests() => AppException(code: 'too-many-requests', message: 'The instance cannot be validated or is invalid.');
 
   static AppException invalidEmail() => AppException(code: 'invalid-email', message: 'Invalid email address.');
 
