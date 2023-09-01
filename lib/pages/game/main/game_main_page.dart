f8033581f8e4c48cbdc20e1d52daaad570e15a58
Wesley LEVASSEUR <mailpro.levasseur@icloud.com>
2023-09-01 12:23:34 +0200

    Update pages.game.main

--- lib/pages/game/main/game_main_page.dart
+++ lib/pages/game/main/game_main_page.dart
@@ -1,9 +1,12 @@
 library game_main;
 
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:fumiko/core/user/core_user.dart';
 import 'package:fumiko/l10n/l10n.dart';
 import 'package:fumiko/models/page_base_model.dart';
+import 'package:fumiko/pages/game/main/widgets/game_header.dart';
+import 'package:fumiko/pages/game/main/widgets/game_subheader.dart';
 import 'package:fumiko/widgets/indicators/fullscreen_double_circular_progress_indicator.dart';
 import 'package:fumiko/widgets/popup/popup_widget.dart';
 import 'package:provider/provider.dart';
--- lib/pages/game/main/game_main_view.dart
+++ lib/pages/game/main/game_main_view.dart
@@ -8,25 +8,42 @@ class _GameMainView extends State<GameMainPage> {
   }
 
   Widget _buildWidget(BuildContext context, _GameMainController controller, Widget? child) {
+    final Widget? tooltipMobileExpProgressIndicator =
+        kIsWeb ? Text('${CoreUser.instance.current.exp}/${CoreUser.instance.current.maxExp}', style: const TextStyle(color: Colors.white, fontSize: 12)) : null;
+    final double mediaWidth = MediaQuery.of(context).size.width;
+    final double mediaHeight = MediaQuery.of(context).size.height;
     return Scaffold(
-        body: Stack(alignment: Alignment.center, children: [
-      PopupWidget(
-          title: controller.popupTitle,
-          titleColor: controller.popupTitleColor,
-          description: controller.popupDescription,
-          icon: controller.popupIcon,
-          iconColor: controller.popupIconColor,
-          presented: controller.popupPresented,
-          buttons: [
-            PopupButtonWidget(color: Colors.red, text: AppLocalizations.of(context).understood, onPressed: controller.closePopup),
-          ],
-          onTap: controller.closePopup,
-          child: FullScreenDoubleCircularProgressIndicator(
-            fontSize: Theme.of(context).textTheme.titleLarge!.fontSize,
-            text: controller.loadingText,
-            presented: controller.isLoading,
-            child: Text(CoreUser.instance.current.username ?? 'X'),
-          ))
-    ]));
+        body: AbsorbPointer(
+            absorbing: !CoreUser.instance.isLoaded || controller.isLoading,
+            child: Stack(alignment: Alignment.center, children: [
+              PopupWidget(
+                  title: controller.popupTitle,
+                  titleColor: controller.popupTitleColor,
+                  description: controller.popupDescription,
+                  icon: controller.popupIcon,
+                  iconColor: controller.popupIconColor,
+                  presented: controller.popupPresented,
+                  buttons: [
+                    PopupButtonWidget(color: Colors.red, text: AppLocalizations.of(context).understood, onPressed: controller.closePopup),
+                  ],
+                  onTap: controller.closePopup,
+                  child: FullScreenDoubleCircularProgressIndicator(
+                    fontSize: Theme.of(context).textTheme.titleLarge!.fontSize,
+                    text: controller.loadingText,
+                    presented: controller.isLoading,
+                    child: SingleChildScrollView(
+                        scrollDirection: Axis.vertical,
+                        child: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            GameHeader(),
+                            GameSubheader(),
+                            Transform.scale(scale: 0, child: Text(CoreUser.instance.current.exp.toString())),
+                          ],
+                        )),
+                  ))
+            ])));
   }
 }
