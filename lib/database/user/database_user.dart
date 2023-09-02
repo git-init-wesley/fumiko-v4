a9c09835af3d980291642a32b5385a1c23e1ccd5
Wesley LEVASSEUR <mailpro.levasseur@icloud.com>
2023-09-02 13:00:13 +0200

    Update DatabaseUser

--- lib/database/user/database_user.dart
+++ lib/database/user/database_user.dart
@@ -4,16 +4,28 @@ import 'dart:async';
 
 import 'package:fumiko/database/database.dart';
 
+part './balance/database_user_balance.dart';
 part './leveling/database_user_leveling.dart';
 
 class DatabaseUserPaths {
   DatabaseUserPaths._();
 
   static username(String uid) => 'users/$uid/username';
+
+  static power(String uid) => 'users/$uid/power';
 }
 
-class DatabaseUser extends _DatabaseUserLeveling with Database {
+class DatabaseUser with Database, _DatabaseUserLeveling, _DatabaseUserBalance {
   String? _uid;
+
+  @override
+  Future<DatabaseResult<V?>> getValue<V>({required String path}) => super.getValue(path: path);
+
+  @override
+  Future<DatabaseResult<V?>> setValue<V>({required String path, required V? value}) => super.setValue(path: path, value: value);
+
+  @override
+  Future<StreamSubscription> addObserver<V>({required String path, required DatabaseListen<V?> listen}) => super.addObserver(path: path, listen: listen);
 
   @override
   String get uid => _uid ?? '...';
@@ -22,9 +34,15 @@ class DatabaseUser extends _DatabaseUserLeveling with Database {
 
   DatabaseUser.of({required String uid}) : _uid = uid;
 
-  Future<String?> getUsername() async => (await super.getValue<String>(path: DatabaseUserPaths.username(uid))).value;
+  Future<DatabaseResult<String?>> getUsername() async => await super.getValue<String>(path: DatabaseUserPaths.username(uid));
 
-  Future<String?> setUsername(String? value) async => (await super.setValue<String>(path: DatabaseUserPaths.username(uid), value: value)).value;
+  Future<DatabaseResult<String?>> setUsername(String? value) async => await super.setValue<String>(path: DatabaseUserPaths.username(uid), value: value);
 
   Future<StreamSubscription> observeUsername(DatabaseListen<String?> listen) async => await super.addObserver<String?>(path: DatabaseUserPaths.username(uid), listen: listen);
+
+  Future<DatabaseResult<num?>> getPower() async => await getValue<num?>(path: DatabaseUserPaths.power(uid));
+
+  Future<DatabaseResult<num?>> setPower(num? newPower) async => await setValue<num?>(path: DatabaseUserPaths.power(uid), value: newPower);
+
+  Future<StreamSubscription?> observePower(DatabaseListen<num?> listen) async => await addObserver(path: DatabaseUserPaths.power(uid), listen: listen);
 }
