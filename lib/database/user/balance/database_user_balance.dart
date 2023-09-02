7f497f79cc4ea08d53003a6aaf06df3256c8b631
Wesley LEVASSEUR <mailpro.levasseur@icloud.com>
2023-09-02 12:59:28 +0200

    Add DatabaseUserBalance

---
+++ lib/database/user/balance/database_user_balance.dart
@@ -0,0 +1,31 @@
+part of database_user;
+
+class DatabaseUserBalancePaths {
+  DatabaseUserBalancePaths._();
+
+  static primary(String uid) => 'users/$uid/balance/primary';
+
+  static secondary(String uid) => 'users/$uid/balance/secondary';
+}
+
+mixin _DatabaseUserBalance {
+  String get uid;
+
+  Future<DatabaseResult<V?>> getValue<V>({required String path});
+
+  Future<DatabaseResult<V?>> setValue<V>({required String path, required V? value});
+
+  Future<StreamSubscription> addObserver<V>({required String path, required DatabaseListen<V?> listen});
+
+  Future<DatabaseResult<num?>> getPrimary() async => await getValue<num?>(path: DatabaseUserBalancePaths.primary(uid));
+
+  Future<DatabaseResult<num?>> setPrimary(num? newPrimary) async => await setValue<num?>(path: DatabaseUserBalancePaths.primary(uid), value: newPrimary);
+
+  Future<StreamSubscription?> observePrimary(DatabaseListen<num?> listen) async => await addObserver(path: DatabaseUserBalancePaths.primary(uid), listen: listen);
+
+  Future<DatabaseResult<num?>> getSecondary() async => await getValue<num?>(path: DatabaseUserBalancePaths.secondary(uid));
+
+  Future<DatabaseResult<num?>> setSecondary(num? newSecondary) async => await setValue<num?>(path: DatabaseUserBalancePaths.secondary(uid), value: newSecondary);
+
+  Future<StreamSubscription?> observeSecondary(DatabaseListen<num?> listen) async => await addObserver(path: DatabaseUserBalancePaths.secondary(uid), listen: listen);
+}
