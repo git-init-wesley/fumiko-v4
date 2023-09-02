6c5bdbad96b30f332b1c9cde3415fee265a5ce47
Wesley LEVASSEUR <mailpro.levasseur@icloud.com>
2023-09-02 12:59:48 +0200

    Update DatabaseUserLeveling

--- lib/database/user/leveling/database_user_leveling.dart
+++ lib/database/user/leveling/database_user_leveling.dart
@@ -10,24 +10,30 @@ class DatabaseUserLevelingPaths {
   static exp(String uid) => 'users/$uid/leveling/exp';
 }
 
-abstract class _DatabaseUserLeveling with Database {
+mixin _DatabaseUserLeveling {
   String get uid;
 
-  Future<num?> getLevels() async => (await getValue<num?>(path: DatabaseUserLevelingPaths.levels(uid))).value;
+  Future<DatabaseResult<V?>> getValue<V>({required String path});
 
-  Future<num?> setLevels(num? newLevels) async => (await setValue<num?>(path: DatabaseUserLevelingPaths.levels(uid), value: newLevels)).value;
+  Future<DatabaseResult<V?>> setValue<V>({required String path, required V? value});
+
+  Future<StreamSubscription> addObserver<V>({required String path, required DatabaseListen<V?> listen});
+
+  Future<DatabaseResult<num?>> getLevels() async => await getValue<num?>(path: DatabaseUserLevelingPaths.levels(uid));
+
+  Future<DatabaseResult<num?>> setLevels(num? newLevels) async => await setValue<num?>(path: DatabaseUserLevelingPaths.levels(uid), value: newLevels);
 
   Future<StreamSubscription?> observeLevels(DatabaseListen<num?> listen) async => await addObserver(path: DatabaseUserLevelingPaths.levels(uid), listen: listen);
 
-  Future<num?> getRebirths() async => (await getValue<num?>(path: DatabaseUserLevelingPaths.rebirths(uid))).value;
+  Future<DatabaseResult<num?>> getRebirths() async => await getValue<num?>(path: DatabaseUserLevelingPaths.rebirths(uid));
 
-  Future<num?> setRebirths(num? newRebirths) async => (await setValue<num?>(path: DatabaseUserLevelingPaths.rebirths(uid), value: newRebirths)).value;
+  Future<DatabaseResult<num?>> setRebirths(num? newRebirths) async => await setValue<num?>(path: DatabaseUserLevelingPaths.rebirths(uid), value: newRebirths);
 
   Future<StreamSubscription?> observeRebirths(DatabaseListen<num?> listen) async => await addObserver(path: DatabaseUserLevelingPaths.rebirths(uid), listen: listen);
 
-  Future<num?> getExp() async => (await getValue<num?>(path: DatabaseUserLevelingPaths.exp(uid))).value;
+  Future<DatabaseResult<num?>> getExp() async => await getValue<num?>(path: DatabaseUserLevelingPaths.exp(uid));
 
-  Future<num?> setExp(num? newExp) async => (await setValue<num?>(path: DatabaseUserLevelingPaths.exp(uid), value: newExp)).value;
+  Future<DatabaseResult<num?>> setExp(num? newExp) async => await setValue<num?>(path: DatabaseUserLevelingPaths.exp(uid), value: newExp);
 
   Future<StreamSubscription?> observeExp(DatabaseListen<num?> listen) async => await addObserver(path: DatabaseUserLevelingPaths.exp(uid), listen: listen);
 }
