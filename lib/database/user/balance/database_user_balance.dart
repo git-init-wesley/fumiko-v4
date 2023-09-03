09ee2aed35fa5c7e51e0e88a23075475dec7b518
Wesley LEVASSEUR <mailpro.levasseur@icloud.com>
2023-09-03 14:20:52 +0200

    Update database.user.balance
    - Add trophies

--- lib/database/user/balance/database_user_balance.dart
+++ lib/database/user/balance/database_user_balance.dart
@@ -6,6 +6,8 @@ class DatabaseUserBalancePaths {
   static primary(String uid) => 'users/$uid/balance/primary';
 
   static secondary(String uid) => 'users/$uid/balance/secondary';
+
+  static trophies(String uid) => 'users/$uid/balance/trophies';
 }
 
 mixin _DatabaseUserBalance {
@@ -28,4 +30,10 @@ mixin _DatabaseUserBalance {
   Future<DatabaseResult<num?>> setSecondary(num? newSecondary) async => await setValue<num?>(path: DatabaseUserBalancePaths.secondary(uid), value: newSecondary);
 
   Future<StreamSubscription?> observeSecondary(DatabaseListen<num?> listen) async => await addObserver(path: DatabaseUserBalancePaths.secondary(uid), listen: listen);
+
+  Future<DatabaseResult<num?>> getTrophies() async => await getValue<num?>(path: DatabaseUserBalancePaths.trophies(uid));
+
+  Future<DatabaseResult<num?>> setTrophies(num? newTrophies) async => await setValue<num?>(path: DatabaseUserBalancePaths.trophies(uid), value: newTrophies);
+
+  Future<StreamSubscription?> observeTrophies(DatabaseListen<num?> listen) async => await addObserver(path: DatabaseUserBalancePaths.trophies(uid), listen: listen);
 }
