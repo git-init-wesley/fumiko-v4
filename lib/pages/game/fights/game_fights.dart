0ed66d0e551cc0e6a2530c7bd05f9055d8679624
Wesley LEVASSEUR <mailpro.levasseur@icloud.com>
2023-09-03 13:21:56 +0200

    Update GameFights

--- lib/pages/game/fights/game_fights.dart
+++ lib/pages/game/fights/game_fights.dart
@@ -1,20 +1,47 @@
 import 'package:flutter/material.dart';
+import 'package:fumiko/l10n/l10n.dart';
+import 'package:fumiko/pages/game/statique/home_menus_button.dart';
 import 'package:fumiko/widgets/game/buttons/menus_button.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 
 class GameFights extends StatelessWidget {
-  const GameFights({super.key, required this.pageController});
+  const GameFights({super.key, required this.pageController, required this.pvpPage, required this.pvePage});
 
   final PageController pageController;
+  final int pvpPage;
+  final int pvePage;
 
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
-      child: MenusButton(
-        color: Colors.black,
-        iconData: MdiIcons.home,
-        text: 'Accueil',
-        onPressed: () => pageController.animateToPage(0, duration: const Duration(microseconds: 1), curve: Curves.linear),
+      child: Column(
+        mainAxisSize: MainAxisSize.max,
+        children: [
+          HomeMenusButton(pageController: pageController),
+          Container(
+            margin: const EdgeInsets.only(top: 16),
+            child: Column(
+              children: [
+                Container(
+                    margin: const EdgeInsets.only(bottom: 8),
+                    child: MenusButton(
+                      color: Colors.redAccent.withOpacity(0.5),
+                      iconData: MdiIcons.controllerOff,
+                      text: AppLocalizations.of(context).pvp,
+                      // onPressed: () => pageController.animateToPage(pvpPage, duration: const Duration(microseconds: 1), curve: Curves.linear),
+                    )),
+                Container(
+                    margin: const EdgeInsets.only(bottom: 8),
+                    child: MenusButton(
+                      color: Colors.blueAccent,
+                      iconData: MdiIcons.controller,
+                      text: AppLocalizations.of(context).pve,
+                      onPressed: () => pageController.animateToPage(pvePage, duration: const Duration(microseconds: 1), curve: Curves.linear),
+                    )),
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
