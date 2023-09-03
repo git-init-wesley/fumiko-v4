d05363c06228d2831184d9589905ebdcd622f2b8
Wesley LEVASSEUR <mailpro.levasseur@icloud.com>
2023-09-03 13:22:06 +0200

    Update GameHome

--- lib/pages/game/home/game_home.dart
+++ lib/pages/game/home/game_home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:fumiko/l10n/l10n.dart';
 import 'package:fumiko/widgets/game/buttons/menus_button.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 
@@ -22,20 +23,20 @@ class GameHome extends StatelessWidget {
               maxWidth: 384,
               color: Colors.redAccent,
               iconData: MdiIcons.sword,
-              text: 'Combats',
+              text: AppLocalizations.of(context).fights,
               textSize: Theme.of(context).textTheme.headlineSmall!.fontSize,
               onPressed: () => pageController.animateToPage(fightsPage, duration: const Duration(microseconds: 1), curve: Curves.linear),
             )),
-        Container(margin: const EdgeInsets.only(bottom: 8), child: MenusButton(color: Colors.tealAccent.shade100, iconData: MdiIcons.sack, text: 'Inventaire')),
-        Container(margin: const EdgeInsets.only(bottom: 8), child: MenusButton(color: Colors.deepPurpleAccent.shade100, iconData: MdiIcons.scriptText, text: 'Quêtes')),
-        Container(margin: const EdgeInsets.only(bottom: 8), child: MenusButton(color: Colors.deepOrangeAccent.shade100, iconData: MdiIcons.map, text: 'Carte')),
-        Container(margin: const EdgeInsets.only(bottom: 8), child: MenusButton(color: Colors.pinkAccent.shade100, iconData: MdiIcons.shield, text: 'Alliance')),
-        Container(margin: const EdgeInsets.only(bottom: 8), child: MenusButton(color: Colors.yellowAccent.shade100, iconData: MdiIcons.shopping, text: 'Boutique')),
-        Container(margin: const EdgeInsets.only(bottom: 8), child: MenusButton(color: Colors.orange.shade100, iconData: MdiIcons.treasureChest, text: 'Coffres')),
+        Container(margin: const EdgeInsets.only(bottom: 8), child: MenusButton(color: Colors.tealAccent.shade100, iconData: MdiIcons.sack, text: AppLocalizations.of(context).inventory)),
+        Container(margin: const EdgeInsets.only(bottom: 8), child: MenusButton(color: Colors.deepPurpleAccent.shade100, iconData: MdiIcons.scriptText, text: AppLocalizations.of(context).quests)),
+        Container(margin: const EdgeInsets.only(bottom: 8), child: MenusButton(color: Colors.deepOrangeAccent.shade100, iconData: MdiIcons.map, text: AppLocalizations.of(context).map)),
+        Container(margin: const EdgeInsets.only(bottom: 8), child: MenusButton(color: Colors.pinkAccent.shade100, iconData: MdiIcons.shield, text: AppLocalizations.of(context).alliance)),
+        Container(margin: const EdgeInsets.only(bottom: 8), child: MenusButton(color: Colors.yellowAccent.shade100, iconData: MdiIcons.shopping, text: AppLocalizations.of(context).shop)),
+        Container(margin: const EdgeInsets.only(bottom: 8), child: MenusButton(color: Colors.orange.shade100, iconData: MdiIcons.treasureChest, text: AppLocalizations.of(context).chests)),
         MenusButton(
           color: Colors.black,
           iconData: MdiIcons.cog,
-          text: 'Paramètres',
+          text: AppLocalizations.of(context).settings,
           onPressed: () => pageController.animateToPage(settingsPage, duration: const Duration(microseconds: 1), curve: Curves.linear),
         )
       ]),
