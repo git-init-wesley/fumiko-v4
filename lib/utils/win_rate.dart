e505cdd50e11afa3a98345e290e40295d67513ed
Wesley LEVASSEUR <mailpro.levasseur@icloud.com>
2023-09-13 08:43:23 +0200

    Add utils WinRate

---
+++ lib/utils/win_rate.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:fumiko/entities/monster/monster.dart';
+import 'package:fumiko/entities/user/user.dart';
+
+class WinRate {
+  num? _rate;
+
+  num get rate => _rate ?? 0;
+  bool? _isBoss;
+
+  bool get isBoss => _isBoss ?? false;
+
+  WinRate.ofPvE({required EntityMonster monster, required EntityUser user}) {
+    _isBoss = monster.isBoss;
+    _rate = user.power / monster.power * 100;
+  }
+
+  Color color() {
+    if (rate >= 75) {
+      return Colors.greenAccent;
+    }
+    if (rate >= 50) {
+      return Colors.yellowAccent;
+    }
+    if (rate >= 25) {
+      return Colors.orangeAccent;
+    }
+    if (isBoss) {
+      return Colors.black;
+    }
+    return Colors.redAccent;
+  }
+}
