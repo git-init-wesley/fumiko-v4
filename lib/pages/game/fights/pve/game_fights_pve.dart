75d98ca4bade21ac5701a460f0b0f6abceddd2d4
Wesley LEVASSEUR <mailpro.levasseur@icloud.com>
2023-09-13 08:46:57 +0200

    Update GameFightsPve

--- lib/pages/game/fights/pve/game_fights_pve.dart
+++ lib/pages/game/fights/pve/game_fights_pve.dart
@@ -1,40 +1,63 @@
 import 'package:flutter/material.dart';
+import 'package:fumiko/core/user/core_user.dart';
+import 'package:fumiko/entities/monster/classes/monster_classes.dart';
+import 'package:fumiko/entities/monster/monster.dart';
 import 'package:fumiko/pages/game/main/game_main_page.dart';
 import 'package:fumiko/pages/game/statique/home_menus_button.dart';
+import 'package:fumiko/utils/win_rate.dart';
+import 'package:fumiko/widgets/game/buttons/pve_monster_button.dart';
 
-class GameFightsPvE extends StatelessWidget {
-  const GameFightsPvE({super.key, required this.setSubPageRoute});
+class GameFightsPve extends StatelessWidget {
+  const GameFightsPve({super.key, required this.setSubPageRoute, required this.setAbstractWidget});
 
   final SetSubPageFunction setSubPageRoute;
+  final SetAbstractWidgetFunction setAbstractWidget;
 
-  /// @version 1.0.0
   List<Widget> _generateMonsters() {
-    return [];
+    List<Widget> monsters = [];
+
+    const num minLevel = 1, maxLevel = 10;
+    final MonsterClass classes = MonsterClasses.fromCode('alpha');
+
+    for (num i = minLevel; i <= maxLevel; i++) {
+      final bool isBoss = i % 5 == 0;
+      final EntityMonster monster = EntityMonster.of(levels: i, isBoss: isBoss, classes: classes);
+      monsters.add(Container(
+        margin: const EdgeInsets.symmetric(vertical: 4, horizontal: 8),
+        padding: const EdgeInsets.symmetric(vertical: 4, horizontal: 8),
+        child: PveMonsterButton(
+          isBoss: isBoss,
+          levels: i,
+          title: 'Monster',
+          power: monster.power,
+          classes: monster.classes,
+          winRate: WinRate.ofPvE(monster: monster, user: CoreUser.instance.current),
+          setAbstractWidget: setAbstractWidget,
+          setSubPageRoute: setSubPageRoute,
+        ),
+      ));
+    }
+
+    return monsters;
   }
 
   @override
   Widget build(BuildContext context) {
-    return Column(mainAxisSize: MainAxisSize.max, children: [
+    return Column(mainAxisAlignment: MainAxisAlignment.center, crossAxisAlignment: CrossAxisAlignment.center, mainAxisSize: MainAxisSize.max, children: [
       HomeMenusButton(setSubPageRoute: setSubPageRoute),
       Container(
         constraints: const BoxConstraints(maxWidth: 384),
         padding: const EdgeInsets.symmetric(vertical: 2, horizontal: 4),
         margin: const EdgeInsets.only(top: 4),
-        child: Text('Vous êtes présentement dans la ville de GenOne se situant dans le pays de Alpha.'),
+        child: Text('Vous êtes présentement dans la ville de GenOne se situant dans le pays de Alpha.', textAlign: TextAlign.center),
       ),
       Container(
         constraints: const BoxConstraints(maxWidth: 384),
         padding: const EdgeInsets.symmetric(vertical: 2, horizontal: 4),
         margin: const EdgeInsets.only(top: 4),
-        child: Text('Vous êtes à l\'heure actuelle X aventuriers dans cette ville avec un total de X aventuriers dans le pays.'),
+        child: Text('Vous êtes à l\'heure actuelle X aventuriers dans cette ville avec un total de X aventuriers dans le pays.', textAlign: TextAlign.center),
       ),
-      Container(
-        margin: const EdgeInsets.only(top: 16),
-        padding: const EdgeInsets.symmetric(vertical: 4, horizontal: 8),
-        child: ListBody(
-          children: _generateMonsters(),
-        ),
-      )
+      Container(margin: const EdgeInsets.only(top: 16), padding: const EdgeInsets.symmetric(vertical: 4, horizontal: 8), child: ListBody(children: _generateMonsters())),
     ]);
   }
 }
