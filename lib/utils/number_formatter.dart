ad7f42518c6e7a402745ca25ca4110254109cbd2
Wesley LEVASSEUR <mailpro.levasseur@icloud.com>
2023-09-16 14:33:59 +0200

    Update NumberFormatter

--- lib/utils/number_formatter.dart
+++ lib/utils/number_formatter.dart
@@ -1,5 +1,8 @@
 import 'package:intl/intl.dart';
 
 class NumberFormatter {
-  static String compact(num value) => NumberFormat.compact().format(value);
+  static NumberFormat formatter = NumberFormat.compact();
+
+  static String compact(num value) => formatter.format(value);
+//static String compact(num value) => formatter.format(num.tryParse(value.toStringAsPrecision(2)) ?? 0);
 }
